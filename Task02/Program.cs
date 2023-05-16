9ff6ef883d038b62fe643ee37567b586c1f78465
Denis <denis09754276@gmail.com>
2023-05-16 21:29:14 +0300

    добавил задания

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое натуральное число: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе натуральное число: ");
+int y = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (x > y)
+{
+    max = x;
+    Console.WriteLine($"{max} Больше");
+}
+else
+{
+    max = y;
+    Console.WriteLine($"{max} Больше");
+}
